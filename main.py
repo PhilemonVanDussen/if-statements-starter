3d411b50e3aaa0d3c442899971432874ed9f33c1
PhilemonVanDussen <vandussenph709@northwested.org>
2024-09-30 14:29:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+# PJ VanDussen
+# 9/30/2024
+# If Statements
 
+#Task 1
+superheroes = ['spider man', 'iron man', 'hulk', 'hawkeye', 'thor', 'captain america']
+if 'spider man' in superheroes:
+    print('My favorite superheroe is spider man')
+#Task 2
+quiz_score = 97
+if quiz_score >= 85:
+    print('You got a B or better on your grade.')
