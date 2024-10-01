111034e5037c5a2731a27c9cbc2883146a8176ba
PhilemonVanDussen <vandussenph709@northwested.org>
2024-10-01 13:25:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ if quiz_score >= 85:
     print('You got a B or better on your grade.')
 #Task 3
 quote = 'Great power comes great respesoniblity.'
-if quote.startswitch(M):
+if quote.startswitch('M'):
     print('Your quote starts with a capital M')
 #Task 4
+filename = 'Money.png'
+if filename.endswith('.png'):
+    print('The file name ends with a .png')
+#Task 5
+is_registered = True
+if is_registered:
+    print('You are registered to a Northwestern Michigan College class.')
